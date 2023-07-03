74fc8dab45416858d4460bc7157670cd45ab9fac
axelb <akolovangin@mail.ru>
2023-07-03 15:29:33 +0300

    Task_4 done

---
+++ Task_4.py
@@ -0,0 +1,19 @@
+twenty_numbers = []
+sum_numbers = 0
+
+for i in range(20):
+    number = int(input(f'Введите {i + 1} число: '))
+    twenty_numbers.append(number)
+
+for num in twenty_numbers:
+    sum_numbers += num
+
+max_number = max(twenty_numbers)
+min_number = min(twenty_numbers)
+average_number = sum_numbers / len(twenty_numbers)
+
+print('-' * 20, '\nСписок чисел:', twenty_numbers,
+      '\nНаибольшее число в списке:', max_number,
+      '\nНаименьшее число в списке:', min_number,
+      '\nСумма чисел в списке:', sum_numbers,
+      '\nСреднее арифметическое чисел в списке:', average_number)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import Task_1
 import Task_2
 import Task_3
+import Task_4
